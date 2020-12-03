46f781a38e346379e7860af1cac497e3c5d3a0d7
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-12-04 02:34:58 +0900

    :tada: (frontend) Initial commit

---
+++ apps/frontend/src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from '/@/App.vue'
+import '/@/assets/styles/main.scss'
+import '/@/assets/styles/tailwind.scss'
+
+createApp(App).mount('#app')
---
+++ apps/frontend/tailwind.config.js
@@ -0,0 +1,19 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { join } = require('path')
+const BASE_DIR = join(__dirname, 'src')
+const VUE_FILE = join('**', '*.vue')
+
+module.exports = {
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [join(BASE_DIR, VUE_FILE), join(__dirname, '*.html')],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
