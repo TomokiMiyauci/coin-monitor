e10eb5da095a02087cf45d4217e988c600436e2d
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-12-06 01:26:16 +0900

    :heavy_plus_sign: (frontend) Use ky

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -1,6 +1,13 @@
-import { createApp } from 'vue'
+import { createApp, Plugin } from 'vue'
 import App from '/@/App.vue'
 import '/@/assets/styles/main.scss'
 import '/@/assets/styles/tailwind.scss'
 
-createApp(App).mount('#app')
+import ky from 'ky'
+
+const plugin: Plugin = {
+  install: (app) => {
+    app.provide('$http', ky)
+  },
+}
+createApp(App).use(plugin).mount('#app')
