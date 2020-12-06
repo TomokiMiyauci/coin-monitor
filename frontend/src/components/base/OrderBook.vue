f48b438d49faabf063633ec3d68bed1804a886bc
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-12-06 12:04:04 +0900

    :sparkles: (frontend) Add order book

---
+++ frontend/src/components/base/OrderBook.vue
@@ -0,0 +1,70 @@
+<template>
+  <table class="mx-auto shadow rounded" style="display: inline-table">
+    <thead>
+      <tr>
+        <th
+          class="px-6 py-3 bg-gray-50 text-center text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+        >
+          Rate
+        </th>
+        <th
+          class="px-6 py-3 bg-gray-50 text-center text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+        >
+          Amount
+        </th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr>
+        <td colspan="2" class="text-center bg-red-500 uppercase p-2">Buy</td>
+      </tr>
+    </tbody>
+
+    <tbody v-for="({ amount, rate }, i) in asks" :key="i">
+      <tr>
+        <td class="px-5 py-1">
+          <comma-filter class="text-red-600" :value="rate" />
+        </td>
+        <td class="px-5">{{ amount }}</td>
+      </tr>
+    </tbody>
+
+    <tbody v-for="({ amount, rate }, i) in bids" :key="i">
+      <tr>
+        <td class="px-5 py-1">
+          <comma-filter class="text-green-500" :value="rate" />
+        </td>
+        <td class="px-5">{{ amount }}</td>
+      </tr>
+    </tbody>
+
+    <tbody>
+      <tr>
+        <td colspan="2" class="text-center bg-green-500 p-2">Sell</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import CommaFilter from '/@/components/base/CommaFilter.vue'
+
+  export default defineComponent({
+    components: {
+      CommaFilter,
+    },
+    props: {
+      asks: {
+        type: Array,
+        default: () => [],
+      },
+
+      bids: {
+        type: Array,
+        default: () => [],
+      },
+    },
+  })
+</script>
--- frontend/src/components/coincheck/CurrentBoard.vue
+++ frontend/src/components/coincheck/CurrentBoard.vue
@@ -1,24 +1,65 @@
 <template>
   <div class="p-2 grid grid-flow-col gap-4">
-    <latest-price :value="value.last" />
-    <ask-bid :ask="value.ask" :bid="value.bid" />
+    <latest-price v-if="result" :value="result.last" />
+    <ask-bid v-if="result" :ask="result.ask" :bid="result.bid" />
   </div>
+
+  <order-book v-if="data.length" :asks="buys" :bids="sells" />
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue'
+  import { defineComponent, ref, computed, onBeforeMount } from 'vue'
   import LatestPrice from '/@/components/base/LatestPrice.vue'
-  import AskBid from '../base/AskBid.vue'
+  import AskBid from '/@/components/base/AskBid.vue'
+  import { useKy } from '/@/plugins/ky'
+  import OrderBook from '/@/components/base/OrderBook.vue'
 
   export default defineComponent({
     components: {
       LatestPrice,
       AskBid,
+      OrderBook,
     },
-    props: {
-      value: {
-        type: Object,
-      },
+
+    setup() {
+      const { $http } = useKy()
+      const data = ref([])
+      const result = ref()
+
+      const sells = computed(() =>
+        data.value.filter(({ order_type }) => order_type === 'sell')
+      )
+      const buys = computed(() =>
+        data.value.filter(({ order_type }) => order_type === 'buy')
+      )
+
+      onBeforeMount(() => {
+        setInterval(setData, 10000)
+        setInterval(setTicker, 10000)
+      })
+
+      const setTicker = async () => {
+        const r = await $http
+          .get('https://coin-monitor-api.tomoki-miyauci.vercel.app/api/src')
+          .json()
+
+        result.value = r
+      }
+
+      const setData = async () => {
+        const { data: d } = await $http
+          .get(
+            'https://coin-monitor-api.tomoki-miyauci.vercel.app/api/src/coincheck/ticker'
+          )
+          .json()
+
+        data.value = d
+      }
+
+      setData()
+      setTicker()
+
+      return { sells, buys, result, data }
     },
   })
 </script>
