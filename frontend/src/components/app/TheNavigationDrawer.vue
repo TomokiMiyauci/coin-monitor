6171ac7371cfda3f0e478283ae07ac83aaf5bd41
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-12-06 11:58:58 +0900

    :lipstick: (frontend) Improve UI

--- frontend/src/components/app/TheNavigationDrawer.vue
+++ frontend/src/components/app/TheNavigationDrawer.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="w-64 bg-gray-900">
-    <div class="flex items-center justify-center mt-8">
+  <div class="w-64 bg-gray-800 p-4">
+    <div class="flex items-center justify-center mt-4">
       <div class="flex items-center">
         <span class="text-white text-2xl mx-2 font-semibold">Coin Monitor</span>
       </div>
     </div>
 
     <nav class="mt-10">
+      <h5 class="flex font-medium text-sm text-gray-400 px-4 my-4 uppercase">
+        markets
+      </h5>
       <a
-        class="flex items-center mt-4 py-2 px-6 bg-gray-700 bg-opacity-25 text-gray-100"
+        class="flex rounded items-center mt-4 py-2 px-6 hover:bg-gray-700 bg-opacity-25 text-gray-100"
         href="/"
       >
         <span class="mx-3">Coincheck</span>
