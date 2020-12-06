bdbfc6b3a70d2dca7fbd0d6c3b822b7adb314809
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-12-07 02:54:10 +0900

    :wrench: (frontend) Add svg plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,12 @@
 import { join } from 'path'
 import type { UserConfig } from 'vite'
-
+const svgPlugin = require('vite-plugin-svg')
 const config: UserConfig = {
   alias: {
     '/@/': join(__dirname, 'src'),
   },
+
+  plugins: [svgPlugin()],
 }
 
 export default config
