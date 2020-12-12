9ed7f8bfb843d1859abf65fa3483cfe937a13792
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-12-12 15:59:19 +0900

    :sparkles: (frontend) Add basic components

---
+++ frontend/src/components/base/BaseCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <base-div
+    class="rounded-md bg-white shadow p-6 hover:shadow-md transition-shadow duration-200"
+  >
+    <slot />
+  </base-div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import BaseDiv from '/@/components/base/BaseDiv.vue'
+  export default defineComponent({
+    components: {
+      BaseDiv,
+    },
+  })
+</script>
