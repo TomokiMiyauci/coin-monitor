c2e302a1b4eb8667c00c7edd2dad0a716f55aa64
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-12-13 19:44:07 +0900

    :recycle: (frontend) Split component

---
+++ frontend/src/components/coincheck/TradeHistory.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h3 class="py-3 px-3 text-lg text-gray-400">Trade History</h3>
+
+    <transition name="fade">
+      <trade-historys v-if="data" class="w-full mt-2" :data="data" />
+      <spin-loader v-else class="h-full" />
+    </transition>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import SpinLoader from '../base/loaders/SpinLoader.vue'
+  import { useTrades } from '/@/components/coincheck/useTrades'
+  import TradeHistorys from '/@/components/trade-history/TradeHistory.vue'
+
+  export default defineComponent({
+    components: {
+      TradeHistorys,
+      SpinLoader,
+    },
+
+    setup() {
+      const { data } = useTrades()
+
+      return { data }
+    },
+  })
+</script>
+
+<style scoped>
+  .fade-enter-active,
+  .fade-leave-active {
+    @apply transition-opacity duration-100;
+  }
+
+  .fade-enter-from,
+  .fade-leave-to {
+    @apply opacity-0;
+  }
+</style>
