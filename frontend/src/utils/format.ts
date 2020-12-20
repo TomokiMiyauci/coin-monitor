40132e90936a639d6207aec60c1781163e0b99cd
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-12-20 15:39:26 +0900

    :sparkles: (frontend) Add format utility

---
+++ frontend/src/utils/format.ts
@@ -0,0 +1,14 @@
+export const toUpperCase = <T extends string>(text: T): Uppercase<T> =>
+  text.toUpperCase() as Uppercase<T>
+export const toLowerCase = <T extends string>(text: T): Lowercase<T> =>
+  text.toLowerCase() as Lowercase<T>
+
+export const joinSeparator = <T extends string, U extends string>(
+  text1: T,
+  text2: U
+) => `${text1}_${text2}` as const
+
+export const getLowercasePair = <T extends string, U extends string>(
+  symbol: T,
+  baseSymbol: U
+): Lowercase<`${T}_${U}`> => toLowerCase(joinSeparator(symbol, baseSymbol))
