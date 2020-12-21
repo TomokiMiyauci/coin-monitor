3cb410e30eeb7fc066154bf323f25e3bcb7c4d52
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-12-21 23:23:17 +0900

    :mag: Add manifest json

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,29 @@
 import { join } from 'path'
 import type { UserConfig } from 'vite'
 import svgPlugin from 'vite-plugin-svg'
+import { VitePWA } from 'vite-plugin-pwa'
+import { name, description } from '../package.json'
+
+const PROJECT_NAME = name
+  .split('-')
+  .map((str) => str.charAt(0).toUpperCase() + str.slice(1))
+  .join(' ')
 
 const config: UserConfig = {
   alias: {
     '/@/': join(__dirname, 'src'),
   },
 
-  plugins: [svgPlugin()],
+  plugins: [
+    svgPlugin(),
+    VitePWA({
+      manifest: {
+        name: PROJECT_NAME,
+        short_name: PROJECT_NAME,
+        description,
+      },
+    }),
+  ],
 
   shouldPreload: () => true,
 }
