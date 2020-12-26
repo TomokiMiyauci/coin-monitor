945b831c9e3b43f4f4cb7cd9cf53df829c9a8278
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-12-26 23:52:18 +0900

    :recycle: (frontend) Change route

---
+++ frontend/src/pages/coincheck.vue
@@ -0,0 +1,19 @@
+<template>
+  <default title="coincheck">
+    <CurrentBoard />
+  </default>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+
+  import Default from '/@/layouts/default.vue'
+  import CurrentBoard from '/@/components/coincheck/CurrentBoard.vue'
+
+  export default defineComponent({
+    components: {
+      Default,
+      CurrentBoard,
+    },
+  })
+</script>
