b9046a1336938275d02a310f1b84e86873b20c68
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-12-28 01:53:17 +0900

    :sparkles: (frontend) Add route

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const Index = () => import('/@/pages/index.vue')
+const Coincheck = () => import('/@/pages/coincheck.vue')
 const Zaif = () => import('/@/pages/zaif.vue')
 const routes = [
   { path: '/', component: Index },
+  { path: '/coincheck', component: Coincheck },
   { path: '/zaif', component: Zaif },
 ]
 export const routerHistory = createWebHistory()
