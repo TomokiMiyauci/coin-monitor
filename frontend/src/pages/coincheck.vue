49890fd785161eb829e3ccf1a7054bbb75363988
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-12-28 01:39:10 +0900

    :sparkles: (frontend) Use firestore

--- frontend/src/pages/coincheck.vue
+++ frontend/src/pages/coincheck.vue
@@ -9,11 +9,25 @@
 
   import Default from '/@/layouts/default.vue'
   import CurrentBoard from '/@/components/coincheck/CurrentBoard.vue'
+  import { doc, getDoc } from 'firebase/firestore/lite'
+  import { useFirestore } from '/@/plugins/firebase'
 
   export default defineComponent({
     components: {
       Default,
       CurrentBoard,
+    },
+
+    setup() {
+      const { $firestore } = useFirestore()
+      getDoc(
+        doc(
+          $firestore,
+          'markets/coincheck/pairs/btc_jpy/rates/1IsLJEVauQfi4LAsiy8E'
+        )
+      ).then((e) => {
+        console.log(e.data())
+      })
     },
   })
 </script>
