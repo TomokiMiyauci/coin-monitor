8d2b8983bbf1603617d7685312837fe4092555d6
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-12-30 01:51:02 +0900

    :wrench: (frontend) Add tailwind extend

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,7 +10,16 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      strokeWidth: {
+        3: '3',
+        4: '4',
+        5: '5',
+        6: '6',
+        7: '7',
+        8: '8',
+      },
+    },
   },
   variants: {
     extend: {},
