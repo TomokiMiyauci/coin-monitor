bd391454bb4b520d15d89b8582a3c5c0e5e1b59b
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-12-29 23:39:39 +0900

    :label: (frontend) Add types

--- frontend/src/api/coincheck/ticker.ts
+++ frontend/src/api/coincheck/ticker.ts
@@ -1,7 +1,13 @@
 import { useKy } from '/@/plugins/ky'
+import type { getAPITicker } from 'coincheck-client'
+type Data = ReturnType<typeof getAPITicker> extends Promise<infer data>
+  ? data
+  : never
 
 export default () => {
   const { $http } = useKy()
 
-  return { get: () => $http.get('api/src/coincheck/ticker').json() }
+  return {
+    get: () => $http.get('api/src/coincheck/ticker').json<Data>(),
+  }
 }
--- frontend/src/components/coincheck/useTicker.ts
+++ frontend/src/components/coincheck/useTicker.ts
@@ -1,21 +1,30 @@
 import { ref, computed } from 'vue'
 import { getTicker } from '/@/api/coincheck'
 import { useInterval } from '/@/core/interval'
+import type { getAPITicker } from 'coincheck-client'
 
+type Data = ReturnType<typeof getAPITicker> extends Promise<infer data>
+  ? data
+  : never
+
+type NumberOrUndefined = number | undefined
 export const useTicker = () => {
-  const state = ref()
+  const state = ref<Data>()
   const { get } = getTicker()
 
   const setData = async () => {
     state.value = await get()
   }
 
-  const last = computed(() => state.value?.last)
-  const ask = computed(() => state.value?.ask)
-  const bid = computed(() => state.value?.bid)
+  const last = computed<NumberOrUndefined>(() => state.value?.last)
+  const ask = computed<NumberOrUndefined>(() => state.value?.ask)
+  const bid = computed<NumberOrUndefined>(() => state.value?.bid)
+  const high = computed<NumberOrUndefined>(() => state.value?.high)
+  const low = computed<NumberOrUndefined>(() => state.value?.low)
+  const volume = computed<NumberOrUndefined>(() => state.value?.volume)
 
   setData()
   useInterval(setData, 10000)
 
-  return { setData, last, ask, bid }
+  return { setData, last, ask, bid, high, low, volume }
 }
