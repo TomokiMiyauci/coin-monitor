8fc652b85f703805627570d86206d4a6eb0f32be
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-12-31 01:45:22 +0900

    :bento: (frontend) Add new img

---
+++ frontend/src/components/base/icons/markets/Bitpoint.vue
@@ -0,0 +1,9 @@
+<template>
+  <img src="/@/assets/img/markets/bitpoint.png" />
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({})
+</script>
