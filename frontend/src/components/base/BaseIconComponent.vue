fd109bab63107603c5d34a80ad9aa62533731de6
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-12-31 01:51:51 +0900

    :sparkles: (frontend) Add generic components

---
+++ frontend/src/components/base/BaseIconComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <component
+    :is="component"
+    :width="width"
+    :height="height"
+    class="inline-block"
+  />
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({
+    props: {
+      width: {
+        type: [String, Number],
+        default: 24,
+      },
+      height: {
+        type: [String, Number],
+        default: 24,
+      },
+      component: {
+        type: Object,
+        requried: true,
+      },
+    },
+  })
+</script>
---
+++ frontend/src/components/base/SvgText.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="flex items-center">
+    <base-icon-component v-bind="baseIconAttrs" />
+    <span class="ml-3">{{ value }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+  import { computed, defineComponent } from 'vue'
+  import BaseIconComponent from '/@/components/base/BaseIconComponent.vue'
+
+  export default defineComponent({
+    components: { BaseIconComponent },
+
+    props: {
+      width: {
+        type: [String, Number],
+        default: 24,
+      },
+      height: {
+        type: [String, Number],
+        default: 24,
+      },
+      callback: {
+        type: Function,
+        required: true,
+      },
+      value: {
+        type: String,
+        required: true,
+      },
+    },
+
+    setup(props) {
+      const component = computed(() => props.callback(props.value))
+      const baseIconAttrs = computed(() => ({
+        component: component.value,
+        width: props.width,
+        height: props.height,
+      }))
+
+      return {
+        baseIconAttrs,
+      }
+    },
+  })
+</script>
