c6fb4b9ec0c0ee3b5f37bab2da2e899ef623b309
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-12-31 01:53:09 +0900

    :sparkles: (frontend) Add ticker composite

---
+++ frontend/src/composites/bitbank/useTicker.ts
@@ -0,0 +1,39 @@
+import { Ref, watch } from 'vue'
+import { getTicker } from '/@/api/bitbank'
+import { useReactive } from '/@/core/reactive'
+import { useInterval } from '/@/core/interval'
+import type { getTicker as data, Pairs } from 'bitbank-client'
+import { computed } from 'vue'
+
+type Data = ReturnType<typeof data> extends Promise<infer data> ? data : never
+
+export const useTicker = (pair: Ref<Pairs>) => {
+  const { get } = getTicker()
+  const { state, setState } = useReactive<Data['data'] | undefined>(undefined)
+
+  const setData = async () => {
+    const { data } = await get(pair.value)
+    setState(data)
+  }
+
+  const ask = computed(() => state.value?.sell)
+  const bid = computed(() => state.value?.buy)
+  const high = computed(() => state.value?.high)
+  const last = computed(() => state.value?.last)
+  const low = computed(() => state.value?.low)
+  const volume = computed(() => state.value?.vol)
+
+  setData()
+
+  watch(pair, setData)
+  useInterval(setData, 10000)
+
+  return {
+    ask,
+    bid,
+    high,
+    low,
+    volume,
+    last,
+  }
+}
