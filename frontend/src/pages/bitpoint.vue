4e3a37ee7974804ceb4c6d23ba66e41518809f04
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-01-01 15:08:23 +0900

    :sparkles: (frontend) Add bitpoint page

---
+++ frontend/src/pages/bitpoint.vue
@@ -0,0 +1,19 @@
+<template>
+  <default title="bitpoint">
+    <CurrentBoard />
+  </default>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+
+  import Default from '/@/layouts/default.vue'
+  import CurrentBoard from '/@/components/bitpoint/CurrentBoard.vue'
+
+  export default defineComponent({
+    components: {
+      Default,
+      CurrentBoard,
+    },
+  })
+</script>
--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -3,10 +3,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 const Index = () => import('/@/pages/index.vue')
 const Coincheck = () => import('/@/pages/coincheck.vue')
 const Zaif = () => import('/@/pages/zaif.vue')
+const Bitpoint = () => import('/@/pages/bitpoint.vue')
 const routes = [
   { path: '/', component: Index },
   { path: '/coincheck', component: Coincheck },
   { path: '/zaif', component: Zaif },
+  { path: '/bitpoint', component: Bitpoint },
 ]
 export const routerHistory = createWebHistory()
 export const router = createRouter({
