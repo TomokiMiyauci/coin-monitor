2fabff5e5b36c3886cb18729a38cce1fafec6581
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-01-01 15:09:27 +0900

    :sparkles: (frontend) Add market icon text

---
+++ frontend/src/components/base/icons/markets/MarketIconText.vue
@@ -0,0 +1,27 @@
+<template>
+  <svg-text :callback="getComponent" :value="value" />
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import { getComponent } from '/@/components/market/symbol'
+  import SvgText from '/@/components/base/SvgText.vue'
+
+  export default defineComponent({
+    components: {
+      SvgText,
+    },
+
+    props: {
+      value: {
+        type: String,
+        default: '',
+      },
+    },
+    setup() {
+      return {
+        getComponent,
+      }
+    },
+  })
+</script>
