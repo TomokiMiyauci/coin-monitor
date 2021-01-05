3a826000fb65e4a4af904790b9029a0b9f498365
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-01-05 22:13:57 +0900

    :sparkles: (frontend) Add market to pair

--- frontend/src/types/pair.ts
+++ frontend/src/types/pair.ts
@@ -1,16 +1,31 @@
-import { zaif, coincheck, bitbank } from '/@/types/market'
+import { zaif, coincheck, bitbank, Markets } from '/@/types/market'
 import type { getTicker } from 'zaif-client'
 import type { getTicker as BitbankPair } from 'bitbank-client'
+import { curry } from '@kahirokunn/ts-curry'
 type Arg<T extends (pair: any) => Promise<any>> = Parameters<T>[number]
 
 export type ZaifPair = Arg<typeof getTicker>
 export type CoincheckPair = ''
 export type BitbankPair = Arg<typeof BitbankPair>
 
-export type Pairs<T> = T extends typeof coincheck
+export type Pairs<T extends Markets> = T extends typeof coincheck
   ? CoincheckPair
   : T extends typeof zaif
   ? ZaifPair
   : T extends typeof bitbank
   ? BitbankPair
   : never
+
+const getMarket2Pair = <T extends Markets>(
+  market: T,
+  pair: Pairs<T>
+): {
+  market: T
+  pair: Pairs<T>
+} => ({ market, pair })
+const curriedMarket2Pair = curry(getMarket2Pair)
+const coincheckPair = curriedMarket2Pair(coincheck)
+const zaifPair = curriedMarket2Pair(zaif)
+const bitbankPair = curriedMarket2Pair(bitbank)
+
+export { coincheckPair, zaifPair, bitbankPair }
