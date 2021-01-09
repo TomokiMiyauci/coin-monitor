1f128eabe4ddc145aaa999d95bfb3388f1a8ef8f
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-01-09 17:30:05 +0900

    :sparkles: (frontend) Add new icon

---
+++ frontend/src/components/base/icons/TextIcon.vue
@@ -0,0 +1,12 @@
+<template>
+  <slot name="text">
+    <span :class="classText" v-show="!hideText">{{ text }}</span>
+  </slot>
+  <slot />
+</template>
+
+<script setup lang="ts">
+  import { defineProps } from 'vue'
+
+  defineProps<{ text?: string; hideText?: boolean; classText?: string }>()
+</script>
---
+++ frontend/src/components/base/icons/mdi/MdiHelpCircle.vue
@@ -0,0 +1,8 @@
+<template>
+  <base-mdi-icon :d="mdiHelpCircle" />
+</template>
+
+<script setup lang="ts">
+  import { mdiHelpCircle } from '@mdi/js'
+  import BaseMdiIcon from '/@/components/base/icons/BaseMdiIcon.vue'
+</script>
