a1da5f454ac5cd750766c7593fb578236710a8fd
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-01-10 03:16:29 +0900

    :sparkles: (frontend) Add utility for asset

---
+++ frontend/src/utils/assert.ts
@@ -0,0 +1,4 @@
+export const isString = (any: any): string => any
+export const isNumber = (any: any): number => any
+export const isStringOrUndefined = (any: any): string | undefined => any
+export const isNumberOrUndefined = (any: any): number | undefined => any
