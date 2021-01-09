cbfc830f80f147f8a1490fffddbb56e3bc631116
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-01-10 03:12:41 +0900

    :sparkles: (frontend) Add generic tooltip

---
+++ frontend/src/components/base/BaseTooltip.vue
@@ -0,0 +1,31 @@
+<template>
+  <span
+    @mouseover="switchHover(true)"
+    @mouseleave="switchHover(false)"
+    class="relative"
+  >
+    <slot />
+
+    <transition name="fade">
+      <base-card class="absolute z-10" :class="classContent" v-show="hover">
+        <slot name="content" />
+      </base-card>
+    </transition>
+  </span>
+</template>
+
+<script setup lang="ts">
+  import { defineProps, ref } from 'vue'
+  import BaseCard from '/@/components/base/BaseCard.vue'
+
+  const hover = ref(false)
+  const switchHover = (isHover: boolean) => {
+    hover.value = isHover
+  }
+
+  defineProps<{ classContent?: string }>()
+</script>
+
+<style scoped lang="scss">
+  @import '../../assets/styles/transitions.scss';
+</style>
