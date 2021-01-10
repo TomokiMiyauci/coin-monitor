11e2575bc9d31943c4797549b722b02c77ba86b1
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-01-10 13:05:50 +0900

    :recycle: (frontend) Merge generic component

--- frontend/src/components/rate/RateTableRatio.vue
+++ frontend/src/components/rate/RateTableRatio.vue
@@ -3,13 +3,17 @@
     <template #header>
       <th class="w-1/2"></th>
       <th></th>
-      <th class="text-right px-3">
-        <text-icon text="1D">
-          <base-tooltip class-content="px-4 py-2 right-0">
+      <th
+        v-for="{ class: className, value, text, tooltipText } in headers"
+        :key="value"
+        :class="className"
+      >
+        <text-icon v-if="text" :text="text">
+          <base-tooltip v-if="tooltipText" class-content="px-4 py-2 right-0">
             <mdi-mdi-help-circle class="w-4 align-top h-4" />
 
             <template #content>
-              <span class="whitespace-nowrap">Now / Yesterday Now</span>
+              <span class="whitespace-nowrap">{{ tooltipText }}</span>
             </template>
           </base-tooltip>
         </text-icon>
@@ -18,17 +22,15 @@
 
     <template #td-symbol="{ symbol }">
       <td class="p-2">
-        <base-svg-symbol :symbol="symbol">
-          <slot name="symbol-icon" :symbol="symbol" />
-        </base-svg-symbol>
+        <slot name="default" :symbol="symbol"> {{ symbol }}</slot>
       </td>
     </template>
 
     <template #expand-td="{ item }">
-      <td class="text-right pr-3">
-        <text-loader :value="item.ratio" skelton-style="min-width: 3rem">
-          <flash-box :value="isNumber(item.ratio)">
-            <percentage :value="isNumber(item.ratio)" />
+      <td v-for="value in headerValues" :key="value" class="text-right pr-3">
+        <text-loader :value="item[value]" skelton-style="min-width: 3rem">
+          <flash-box :value="isNumber(item[value])">
+            <percentage :value="isNumber(item[value])" />
           </flash-box>
         </text-loader>
       </td>
@@ -37,19 +39,26 @@
 </template>
 
 <script setup lang="ts">
-  import { defineProps } from 'vue'
+  import { defineProps, computed } from 'vue'
 
   import BaseRateTable from '/@/components/rate/BaseRateTable.vue'
   import FlashBox from '/@/components/base/FlashBox.vue'
   import TextLoader from '/@/components/base/loaders/TextLoader.vue'
   import Percentage from '/@/components/base/Percentage.vue'
-  import BaseSvgSymbol from '/@/components/base/BaseSvgSymbol.vue'
   import TextIcon from '/@/components/base/icons/TextIcon.vue'
   import MdiMdiHelpCircle from '/@/components/base/icons/mdi/MdiHelpCircle.vue'
   import BaseTooltip from '/@/components/base/BaseTooltip.vue'
   import { isNumber } from '/@/utils/assert'
 
-  defineProps<{
+  const props = defineProps<{
+    headers: {
+      value: string | number
+      text?: string
+      tooltipText?: string
+      class?: string
+    }[]
     items: { symbol: string; rate?: number; ratio?: number }[]
   }>()
+
+  const headerValues = computed(() => props.headers.map(({ value }) => value))
 </script>
