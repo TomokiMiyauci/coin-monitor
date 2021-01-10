eab58e056f67922ac919312e26a9d0a0e2479065
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-01-10 13:06:40 +0900

    :recycle: (frontend) Merge map function

--- frontend/src/components/coincheck/CoincheckRates.vue
+++ frontend/src/components/coincheck/CoincheckRates.vue
@@ -17,20 +17,28 @@
       </base-menu> -->
     </div>
 
-    <rate-table-ratio-ip class="w-full" :items="ccc">
-      <template #symbol-icon="{ symbol }">
-        <coincheck-symbol :symbol="symbol" />
+    <rate-table-ratio
+      class="w-full"
+      :headers="[
+        { value: 'ratioIp', text: '0:00', tooltipText: 'Now / Initial Price' },
+        { value: 'ratio', text: '1D', tooltipText: 'Now / Yesterday Now' },
+      ]"
+      :items="ccc"
+    >
+      <template #default="{ symbol }">
+        <base-svg-symbol :symbol="symbol">
+          <coincheck-symbol :symbol="symbol" />
+        </base-svg-symbol>
       </template>
-    </rate-table-ratio-ip>
+    </rate-table-ratio>
   </base-card>
 </template>
 
-<script lang="ts">
-  import { computed, defineComponent, ref } from 'vue'
+<script setup lang="ts">
+  import { computed, ref } from 'vue'
   import { getPairs } from '/@/components/coincheck/useRate'
   import BaseCard from '/@/components/base/BaseCard.vue'
 
-  import { coincheckSymbols } from '/@/components/base/coin'
   import CoincheckSymbol from '/@/components/coincheck/CoincheckSymbol.vue'
 
   import { useRate } from '/@/reactives/api/coincheck'
@@ -40,182 +48,145 @@
     useOpenPrice,
     useYesterdayNowPrice,
   } from '/@/reactives/api/coincheck'
-  import RateTableRatioIp from '/@/components/rate/RateTableRatioIp.vue'
+  import RateTableRatio from '/@/components/rate/RateTableRatio.vue'
   import { useFirestore } from '/@/plugins/firebase'
+  import BaseSvgSymbol from '/@/components/base/BaseSvgSymbol.vue'
+  import {getLowercasePair} from '/@/utils/format'
+  import type {FirebaseFirestore} from 'firebase/firestore/lite'
+  const { $http } = useKy()
+  const { $firestore } = useFirestore()
+  const now = new Date()
 
-  export default defineComponent({
-    components: {
-      BaseCard,
-      RateTableRatioIp,
-      CoincheckSymbol,
-    },
+  const base = ref<'JPY'>('JPY')
 
-    setup() {
-      const { $http } = useKy()
-      const { $firestore } = useFirestore()
-      const now = new Date()
+  // const onChange = (now: any) => {
+  //   base.value = now
+  //   const pairs = getPairs(now)
+  //   console.log(pairs)
+  //   const m = generateCoincheckPairs(pairs)
+  //   coincheckPairs.forEach(
+  //     ({ coincheckPair, symbol, baseSymbol }, index) => {
+  //       symbol.value = m[index].symbol.value
+  //       baseSymbol.value = now
+  //       coincheckPair.value = m[index].coincheckPair.value
+  //     }
+  //   )
+  // }
 
-      const base = ref('JPY')
+  // const generateCoincheckPairs = (
+  //   pairs: {
+  //     baseSymbol: string
+  //     symbol: string
+  //   }[]
+  // ) => {
+  //   return pairs.map(({ baseSymbol, symbol }) => ({
+  //     coincheckPair: ref(
+  //       `${symbol.toLowerCase()}_${baseSymbol.toLowerCase()}`
+  //     ),
+  //     symbol: ref(symbol),
+  //     baseSymbol: ref(baseSymbol),
+  //   }))
+  // }
 
-      // const onChange = (now: any) => {
-      //   base.value = now
-      //   const pairs = getPairs(now)
-      //   console.log(pairs)
-      //   const m = generateCoincheckPairs(pairs)
-      //   coincheckPairs.forEach(
-      //     ({ coincheckPair, symbol, baseSymbol }, index) => {
-      //       symbol.value = m[index].symbol.value
-      //       baseSymbol.value = now
-      //       coincheckPair.value = m[index].coincheckPair.value
-      //     }
-      //   )
-      // }
+  const pairs = getPairs(base.value)
+  const convertor = (
+    pairs: {
+      baseSymbol: string
+      symbol: string
+    }[]
+  ) => pairs.map(({ baseSymbol, symbol }) => getLowercasePair(symbol, baseSymbol))
+  const lowerPairs = computed(() => convertor(pairs))
 
-      // const generateCoincheckPairs = (
-      //   pairs: {
-      //     baseSymbol: string
-      //     symbol: string
-      //   }[]
-      // ) => {
-      //   return pairs.map(({ baseSymbol, symbol }) => ({
-      //     coincheckPair: ref(
-      //       `${symbol.toLowerCase()}_${baseSymbol.toLowerCase()}`
-      //     ),
-      //     symbol: ref(symbol),
-      //     baseSymbol: ref(baseSymbol),
-      //   }))
-      // }
+  const a = lowerPairs.value.map((pair) => {
+    const { setData, state } = useRate($http, pair)
+    setData()
 
-      const lowerPair = (baseSymbol: string, symbol: string): string =>
-        `${symbol.toLowerCase()}_${baseSymbol.toLowerCase()}`
+    useInterval(setData, 10000)
 
-      const pairs = getPairs(base.value)
-      const convertor = (
-        pairs: {
-          baseSymbol: string
-          symbol: string
-        }[]
-      ) => pairs.map(({ baseSymbol, symbol }) => lowerPair(baseSymbol, symbol))
-      const lowerPairs = computed(() => convertor(pairs))
+    return {
+      state,
+      setData,
+    }
+  })
 
-      const a = lowerPairs.value.map((pair) => {
-        const { setData, state } = useRate($http, pair)
-        setData()
+  const activator = (pair: string, fn: typeof useOpenPrice, date: Date, firestore: FirebaseFirestore) => {
+    const { state, setData } = fn(pair, date, firestore)
+    setData()
 
-        useInterval(setData, 10000)
+    return {
+      state,
+      setData,
+    }
+  }
 
-        return {
-          state,
-          setData,
-        }
-      })
+  const b = lowerPairs.value.map((pair) => activator(pair,useOpenPrice, now, $firestore))
+  const c = lowerPairs.value.map((pair) => activator(pair,useYesterdayNowPrice, now, $firestore))
 
-      const b = lowerPairs.value.map((pair) => {
-        const { state, setData } = useOpenPrice(pair, now, $firestore)
-        setData()
+  const reducer = (acc: {}, cur: ReturnType<typeof activator>) => {
+    if (!cur.state.value) return acc
 
-        return {
-          state,
-          setData,
-        }
-      })
+      acc[cur.state.value.pair] = cur.state.value.value
+      return acc
+}
 
-      const c = lowerPairs.value.map((pair) => {
-        const { state, setData } = useYesterdayNowPrice(pair, now, $firestore)
-        setData()
+  const bbbb = computed(() =>  a.reduce((acc, cur) => {
+     if (!cur.state.value) return acc
 
-        return {
-          state,
-          setData,
-        }
-      })
 
-      const bbbb = computed(() => {
-        return a.reduce((acc, { state }) => {
-          if (!state.value) return acc
+      acc[cur.state.value.pair] = cur.state.value.rate
+      return acc
+  }, {}))
+  const bbb = computed(() =>  b.reduce(reducer, {}))
+  const eee = computed(() =>  c.reduce(reducer, {}))
 
-          acc[state.value.pair] = state.value.rate
-          return acc
-        }, {})
-      })
+  const ccc = computed(() => {
+    return pairs.map(({ baseSymbol, symbol }) => {
+      const p = getLowercasePair(symbol, baseSymbol)
+      const a = (bbb.value[p] as any) as number
+      const e = (eee.value[p] as any) as number
+      const b = (bbbb.value[p] as any) as number
 
-      const bbb = computed(() => {
-        return b.reduce((acc, { state }) => {
-          if (!state.value) return acc
-
-          acc[state.value.pair] = state.value.value
-          return acc
-        }, {})
-      })
-
-      const eee = computed(() => {
-        return c.reduce((acc, { state }) => {
-          if (!state.value) return acc
-
-          acc[state.value.pair] = state.value.value
-          return acc
-        }, {})
-      })
-
-      const ccc = computed(() => {
-        return pairs.map(({ baseSymbol, symbol }) => {
-          const p = lowerPair(baseSymbol, symbol)
-          const a = (bbb.value[p] as any) as number
-          const e = (eee.value[p] as any) as number
-          const b = (bbbb.value[p] as any) as number
-
-          const ratioIp = !!a && !!b ? (b / a - 1) * 100 : undefined
-          const ratio = !!e && !!b ? (b / e - 1) * 100 : undefined
-
-          return {
-            baseSymbol,
-            symbol,
-            rate: bbbb.value[p],
-            ratio,
-            ratioIp,
-          }
-        })
-      })
-
-      // const coincheckPairs = generateCoincheckPairs(pairs)
-
-      // const getRefRateMap = () => {
-      //   return coincheckPairs.map(({ coincheckPair, symbol, baseSymbol }) => {
-      //     const { rate, ratio } = useRate(coincheckPair)
-      //     return {
-      //       rate,
-      //       ratio,
-      //       symbol,
-      //       baseSymbol,
-      //     }
-      //   })
-      // }
-      // const symbolMap = ref(getRefRateMap())
-
-      // type Rate = {
-      //   symbol: string
-      //   baseSymbol: string
-      //   rate: number | undefined
-      //   ratio: number | undefined
-      // }
-
-      // const rates = computed(() =>
-      //   Object.assign(
-      //     {},
-      //     ...symbolMap.value.map(({ rate, symbol, ratio }) => {
-      //       return { [symbol]: { rate, ratio } }
-      //     })
-      //   )
-      // )
+      const ratioIp = !!a && !!b ? (b / a - 1) * 100 : undefined
+      const ratio = !!e && !!b ? (b / e - 1) * 100 : undefined
 
       return {
-        ccc,
-        bbb,
-        // onChange,
-        bbbb,
-        base,
-        coincheckSymbols,
+        baseSymbol,
+        symbol,
+        rate: bbbb.value[p],
+        ratio,
+        ratioIp,
       }
-    },
+    })
   })
-</script>
+
+  // const coincheckPairs = generateCoincheckPairs(pairs)
+
+  // const getRefRateMap = () => {
+  //   return coincheckPairs.map(({ coincheckPair, symbol, baseSymbol }) => {
+  //     const { rate, ratio } = useRate(coincheckPair)
+  //     return {
+  //       rate,
+  //       ratio,
+  //       symbol,
+  //       baseSymbol,
+  //     }
+  //   })
+  // }
+  // const symbolMap = ref(getRefRateMap())
+
+  // type Rate = {
+  //   symbol: string
+  //   baseSymbol: string
+  //   rate: number | undefined
+  //   ratio: number | undefined
+  // }
+
+  // const rates = computed(() =>
+  //   Object.assign(
+  //     {},
+  //     ...symbolMap.value.map(({ rate, symbol, ratio }) => {
+  //       return { [symbol]: { rate, ratio } }
+  //     })
+  //   )
+  // )
+
