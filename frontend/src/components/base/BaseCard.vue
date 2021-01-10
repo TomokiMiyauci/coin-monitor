15187dfc623014f7119ab7440b5a988074001339
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-01-10 13:30:15 +0900

    :fire: (frontend) Remove unused

--- frontend/src/components/base/BaseCard.vue
+++ frontend/src/components/base/BaseCard.vue
@@ -1,15 +1,14 @@
 <template>
-  <base-div
+  <div
     class="rounded-3xl bg-gray-50 bg-gradient-to-b shadow-md hover:shadow-xl transition-shadow duration-200"
     :class="{ 'overflow-hidden': overflowHidden || true }"
   >
     <slot />
-  </base-div>
+  </div>
 </template>
 
 <script setup lang="ts">
   import { defineProps } from 'vue'
-  import BaseDiv from '/@/components/base/BaseDiv.vue'
 
   defineProps<{ overflowHidden?: true }>()
 </script>
