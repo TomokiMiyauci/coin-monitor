1341b853e262dc41da972db43b60419dca8a602a
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-01-19 02:59:05 +0900

    :sparkles: (frontend) Use chartist

---
+++ frontend/src/components/chart/LineChart.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="line-chart" />
+</template>
+
+<script setup lang="ts">
+  import { Line } from 'chartist'
+  import { onMounted, defineProps, watch } from 'vue'
+  type LineChart = ConstructorParameters<typeof Line>
+  type Data = LineChart[1]
+  type Options = LineChart[2]
+
+  const props = defineProps<{
+    data: Data
+    options?: Options
+  }>()
+
+  watch(props, () => {
+    new Line('#line-chart', props.data).update(props.data, props.options)
+  })
+
+  onMounted(() => {
+    new Line('#line-chart', props.data, props.options)
+  })
+</script>
