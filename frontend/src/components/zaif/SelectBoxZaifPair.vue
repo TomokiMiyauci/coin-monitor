83280f3e0fc552ad3515d3a769a388f978a3b63d
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-01-19 23:55:06 +0900

    :sparkles: (frontend) Add select box for zaif pair

---
+++ frontend/src/components/zaif/SelectBoxZaifPair.vue
@@ -0,0 +1,39 @@
+<template>
+  <base-menu
+    :value="value"
+    title="Symbol Pair"
+    class-button="w-52"
+    :candidates="zaifOrderBookPairs"
+    @input="$emit('input', $event)"
+  >
+    <template #buttonContent="{ value }">
+      <base-svg-pair v-bind="format(value)" />
+    </template>
+
+    <template #candidate="{ candidate }">
+      <base-svg-pair v-bind="format(candidate)" />
+    </template>
+  </base-menu>
+</template>
+
+<script setup lang="ts">
+  import { defineProps } from 'vue'
+
+  import BaseSvgPair from '/@/components/base/BaseSvgPair.vue'
+  import { zaifOrderBookPairs } from '/@/components/zaif/pair'
+  import BaseMenu from '/@/components/menu/BaseMenu.vue'
+
+  defineProps<{ value: string }>()
+
+  const format = (payload: string) => {
+    const [symbol, baseSymbol] = payload.split('_')
+    return {
+      symbol: symbol.toUpperCase(),
+      baseSymbol: baseSymbol.toUpperCase(),
+    }
+  }
+
+  // const onInput = (payload: ZaifOrderBookPairs) => {
+  //   pair.value = payload
+  // }
+</script>
