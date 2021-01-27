31da497e9a15b261d3bb805d3e84fef669ad337d
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-01-27 21:46:54 +0900

    :wrench: (frontend) Add vite plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,7 @@ import { resolve } from 'path'
 import { UserConfig } from 'vite'
 import svgPlugin from 'vite-plugin-svg'
 import { name, description } from '../package.json'
-
+import vue from '@vitejs/plugin-vue'
 const PROJECT_NAME = name
   .split('-')
   .map((str) => str.charAt(0).toUpperCase() + str.slice(1))
@@ -14,7 +14,12 @@ const config: UserConfig = {
     '@story': resolve(__dirname, 'stories'),
   },
 
-  plugins: [svgPlugin()],
+  plugins: [
+    vue({
+      ssr: !!process.env.VITE_SSG,
+    }),
+    svgPlugin(),
+  ],
   terserOptions: {
     compress: true,
     mangle: true,
